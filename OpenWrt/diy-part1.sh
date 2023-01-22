f85a78042cb24e143732dd86f2b1f22a52a4105d
易大宝 <yishangfei.me@gmail.com>
2023-01-22 18:12:26 +0800

    Add files via upload

---
+++ OpenWrt/diy-part1.sh
@@ -0,0 +1,3 @@
+# cd $WORK_PATH 目录下,先运行的 public.h -> 设备.h -> scripts/feeds install -a
+# 必须的文件
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
---
+++ OpenWrt/diy-part2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 自定义 sh
+echo "设置lan ip"
+lan_ip='192.168.1.1' 
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+echo "修改时区"
+utc_name='Asia\/Shanghai' 
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+echo "修改机器名称"
+device_name='yishangfei'
+sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
+
+echo "修改wifi名称"
+wifi_name=ysf
+sed -i "s/OpenWrt/$wifi_name/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# echo "修改wifi默认开启"
+# wifi_disabled=0
+# sed -i "s/1/$lan_ip/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
