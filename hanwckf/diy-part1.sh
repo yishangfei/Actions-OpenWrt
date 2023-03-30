ca7a96496c5dcf2e302a2f78a600480d8d33159a
易大宝 <yishangfei.me@gmail.com>
2023-03-30 10:32:30 +0800

    Update diy-part1.sh

--- hanwckf/diy-part1.sh
+++ hanwckf/diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Add a feed source
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
